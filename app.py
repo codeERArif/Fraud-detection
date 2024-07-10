7f290743dfcc7056a21115027b9da3d656adcc21
codeERArif <96327896+codeERArif@users.noreply.github.com>
2024-07-10 09:51:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+import pickle
+import pandas
+import numpy as np
+import sklearn
+import imblearn
+import streamlit as st
+from PIL import Image
+
+st.title("Credit Card Fraud Detection Model")
+
+st.image("fraud.png")
+
+input_df = st.text_input("Please provide all the required feature details: ")
+input_df_split = input_df.split(',')
+print(input_df)
+
+submit = st.button("Submit")
+
+if submit:
+    model = pickle.load(open('model.pkl', 'rb'))
+    features = np.asarray(input_df_split, dtype=np.float64)
+    prediction = model.predict(features.reshape(1, -1))
+
+    if prediction[0] == 0:
+        st.write("Legitimate Transaction")
+    else:
+        st.write("Fraudulent Transaction")
